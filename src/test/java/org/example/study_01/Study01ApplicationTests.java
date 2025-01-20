a3312366121412d7618c5cca17c807fa473a8e93
Choi Dongin <di0308@naver.com>
2025-01-20 18:03:29 +0900

    test: question jpa test

--- src/test/java/org/example/study_01/Study01ApplicationTests.java
+++ src/test/java/org/example/study_01/Study01ApplicationTests.java
@@ -1,13 +1,26 @@
 package org.example.study_01;
 
+import org.example.study_01.mysite.sbb.Question;
+import org.example.study_01.mysite.sbb.QuestionRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class Study01ApplicationTests {
 
+    @Autowired
+    private QuestionRepository questionRepository;
+
     @Test
-    void contextLoads() {
+    void testJpa() {
+        Question q1 = new Question();
+        q1.setSubject("sbb가 무엇인가요?");
+        q1.setContent("sbb에 대해서 알고 싶습니다.");
+        q1.setCreatedAt(LocalDateTime.now());
+        this.questionRepository.save(q1);  // 첫번째 질문 저장
     }
 
 }
