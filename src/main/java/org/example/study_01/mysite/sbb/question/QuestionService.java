5650b1c6e2b1d4807baa43a97be78a98cf956534
gyeolhan0789 <gyeolhan0789@gmail.com>
2025-01-22 17:11:04 +0900

    2-10 상세 페이지 만들기

--- src/main/java/org/example/study_01/mysite/sbb/question/QuestionService.java
+++ src/main/java/org/example/study_01/mysite/sbb/question/QuestionService.java
@@ -1,7 +1,9 @@
 package org.example.study_01.mysite.sbb.question;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.example.study_01.mysite.sbb.DataNotFoundException;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -12,7 +14,12 @@ public class QuestionService {
 
     private final QuestionRepository questionRepository;
 
-    public List<Question> getList() {
-        return this.questionRepository.findAll();
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
