e4c23af9c1cf3833aa1754505fb1dfac24eedb3c
ukjinSPACE <dnrwls128@gmail.com>
2025-01-30 18:06:16 +0900

    3-05 스프링 시큐리티란?

--- src/main/java/org/example/study_01/mysite/sbb/question/QuestionController.java
+++ src/main/java/org/example/study_01/mysite/sbb/question/QuestionController.java
@@ -16,7 +16,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import jakarta.validation.Valid;
 import org.springframework.validation.BindingResult;
 
+import org.springframework.data.domain.Page;
+
 import org.example.study_01.mysite.sbb.answer.AnswerForm;
+
 
 @RequestMapping("/question") // 프리픽스
 @RequiredArgsConstructor
@@ -37,11 +40,15 @@ public class QuestionController {
 
     // 2-9 질문 목록 반환
     @GetMapping("/list") // question 빼고 뒷부분만
-    public String questionList(Model model) {
+    public String questionList(Model model, @RequestParam(value="page", defaultValue="0") int page) {
         // 서비스에서 만든 메서드를 사용
-        List<Question> questionList = this.questionService.getList();
+        //List<Question> questionList = this.questionService.getList();
+        //model.addAttribute("questionList", questionList);
 
-        model.addAttribute("questionList", questionList);
+        // 3-2. 페이징 처리
+        Page<Question> paging = this.questionService.getList(page);
+        model.addAttribute("paging", paging);
+
         return "question_list";
     }
 
@@ -69,4 +76,5 @@ public class QuestionController {
         this.questionService.create(questionForm.getSubject(), questionForm.getContent());
         return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
     }
+
 }
