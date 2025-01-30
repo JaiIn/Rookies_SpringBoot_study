a173def66389bdd93d459e616c4b4ff50bbc91a6
ukjinSPACE <dnrwls128@gmail.com>
2025-01-31 01:33:43 +0900

    3-06 회원가입기능

---
+++ src/main/java/org/example/study_01/mysite/sbb/user/UserRepository.java
@@ -0,0 +1,6 @@
+package org.example.study_01.mysite.sbb.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<SiteUser, Long> {
+}
