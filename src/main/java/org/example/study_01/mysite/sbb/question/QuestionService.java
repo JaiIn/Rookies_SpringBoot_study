c1bbfc4c8712bdb9939e0235fe36b430d3106076
gyeolhan0789 <gyeolhan0789@gmail.com>
2025-02-02 00:42:09 +0900

    Feat: 2-16 질문 등록 기능 추가하기

--- src/main/java/org/example/study_01/mysite/sbb/question/QuestionService.java
+++ src/main/java/org/example/study_01/mysite/sbb/question/QuestionService.java
@@ -1,7 +1,9 @@
 package org.example.study_01.mysite.sbb.question;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.time.LocalDateTime;
 
 import org.example.study_01.mysite.sbb.DataNotFoundException;
 import org.springframework.stereotype.Service;
@@ -25,5 +27,13 @@ public class QuestionService {
         } else {
             throw new DataNotFoundException("question not found");
         }
+    }
+
+    public void create(String subject, String content) {
+        Question q = new Question();
+        q.setSubject(subject);
+        q.setContent(content);
+        q.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q);
     }
 }
