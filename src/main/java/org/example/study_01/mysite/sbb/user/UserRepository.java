af3957014a9a947724139d9630a416d872c374ee
gyeolhan0789 <gyeolhan0789@gmail.com>
2025-02-02 01:47:59 +0900

    Feat: 3-07 로그인과 로그아웃 기능 구현하기

--- src/main/java/org/example/study_01/mysite/sbb/user/UserRepository.java
+++ src/main/java/org/example/study_01/mysite/sbb/user/UserRepository.java
@@ -1,6 +1,9 @@
 package org.example.study_01.mysite.sbb.user;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<SiteUser, Long> {
+    Optional<SiteUser> findByusername(String username);
 }
